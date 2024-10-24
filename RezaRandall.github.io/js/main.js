57fdd66e725f9726ec975bca8eb0b006fb970597
rezarandall <dwirezairawan07@gmail.com>
2024-10-24 22:10:34 +0700

    restore delete files

---
+++ RezaRandall.github.io/js/main.js
@@ -0,0 +1,217 @@
+(function () {
+  "use strict";
+
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  var isMobile = {
+    Android: function () {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+      return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+    },
+  };
+
+  var fullHeight = function () {
+    if (!isMobile.any()) {
+      $(".js-fullheight").css("height", $(window).height());
+      $(window).resize(function () {
+        $(".js-fullheight").css("height", $(window).height());
+      });
+    }
+  };
+
+  // Parallax
+  var parallax = function () {
+    $(window).stellar();
+  };
+
+  var contentWayPoint = function () {
+    var i = 0;
+    $(".animate-box").waypoint(
+      function (direction) {
+        if (direction === "down" && !$(this.element).hasClass("animated-fast")) {
+          i++;
+
+          $(this.element).addClass("item-animate");
+          setTimeout(function () {
+            $("body .animate-box.item-animate").each(function (k) {
+              var el = $(this);
+              setTimeout(
+                function () {
+                  var effect = el.data("animate-effect");
+                  if (effect === "fadeIn") {
+                    el.addClass("fadeIn animated-fast");
+                  } else if (effect === "fadeInLeft") {
+                    el.addClass("fadeInLeft animated-fast");
+                  } else if (effect === "fadeInRight") {
+                    el.addClass("fadeInRight animated-fast");
+                  } else {
+                    el.addClass("fadeInUp animated-fast");
+                  }
+
+                  el.removeClass("item-animate");
+                },
+                k * 100,
+                "easeInOutExpo"
+              );
+            });
+          }, 50);
+        }
+      },
+      { offset: "85%" }
+    );
+  };
+
+  var goToTop = function () {
+    $(".js-gotop").on("click", function (event) {
+      event.preventDefault();
+
+      $("html, body").animate(
+        {
+          scrollTop: $("html").offset().top,
+        },
+        500,
+        "easeInOutExpo"
+      );
+
+      return false;
+    });
+
+    $(window).scroll(function () {
+      var $win = $(window);
+      if ($win.scrollTop() > 200) {
+        $(".js-top").addClass("active");
+      } else {
+        $(".js-top").removeClass("active");
+      }
+    });
+  };
+
+  var pieChart = function () {
+    $(".chart").easyPieChart({
+      scaleColor: false,
+      lineWidth: 4,
+      lineCap: "butt",
+      barColor: "#FF9000",
+      trackColor: "#f5f5f5",
+      size: 160,
+      animate: 1000,
+    });
+  };
+
+  var skillsWayPoint = function () {
+    if ($("#fh5co-skills").length > 0) {
+      $("#fh5co-skills").waypoint(
+        function (direction) {
+          if (direction === "down" && !$(this.element).hasClass("animated")) {
+            setTimeout(pieChart, 400);
+            $(this.element).addClass("animated");
+          }
+        },
+        { offset: "90%" }
+      );
+    }
+  };
+
+  // Loading page
+  var loaderPage = function () {
+    $(".fh5co-loader").fadeOut("slow");
+  };
+
+  $(function () {
+    contentWayPoint();
+    goToTop();
+    loaderPage();
+    fullHeight();
+    parallax();
+    // pieChart();
+    skillsWayPoint();
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener("load", () => {
+    let portfolioContainer = select(".portfolio-container");
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: ".portfolio-item",
+      });
+
+      let portfolioFilters = select("#portfolio-flters li", true);
+
+      portfolioFilters.forEach(function (filter) {
+        filter.addEventListener("click", function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          filter.classList.add("filter-active");
+
+          portfolioIsotope.arrange({
+            filter: filter.getAttribute("data-filter"),
+          });
+          portfolioIsotope.on("arrangeComplete", function () {
+            AOS.refresh();
+          });
+        });
+      });
+    }
+  });
+
+  /**
+   * Initiate portfolio lightbox
+   */
+  const portfolioLightbox = GLightbox({
+    selector: ".portfolio-lightbox",
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper(".portfolio-details-slider", {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener("load", () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  });
+})();
